768b3547aa16634669ac4f018bdf164802e94e55
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-06-12 17:53:40 +0200

    custom app bar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/shared/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -33,16 +34,23 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
-    return nowPlayingMovies.length == 0
-        ? CircularProgressIndicator()
-        : ListView.builder(
-            itemCount: nowPlayingMovies.length,
-            itemBuilder: (context, index) {
-              final movie = nowPlayingMovies[index];
-              return ListTile(
-                title: Text(movie.title),
-              );
-            },
+    return nowPlayingMovies.isEmpty
+        ? const Center(child: CircularProgressIndicator())
+        : Column(
+            children: [
+              CustomAppBar(),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: nowPlayingMovies.length,
+                  itemBuilder: (context, index) {
+                    final movie = nowPlayingMovies[index];
+                    return ListTile(
+                      title: Text(movie.title),
+                    );
+                  },
+                ),
+              ),
+            ],
           );
   }
 }
