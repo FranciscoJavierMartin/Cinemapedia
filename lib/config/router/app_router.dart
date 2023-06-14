984f89811b92258eb35493ecb672de147e049d70
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-06-14 19:12:10 +0200

    Create detail movie screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,8 +3,15 @@ import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(
-    path: '/',
-    name: HomeScreen.name,
-    builder: (context, state) => const HomeScreen(),
-  ),
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) =>
+              MovieScreen(movieId: state.pathParameters['id'] ?? 'no-id'),
+        ),
+      ]),
 ]);
