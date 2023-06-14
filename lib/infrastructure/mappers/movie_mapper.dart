0d92f5c61727ce1ede3cd884a05fc547c7c4ebf6
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-06-14 19:42:21 +0200

    Get movie details by id

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/themoviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/themoviedb/movie_themoviedb.dart';
 
 class MovieMapper {
@@ -8,6 +9,27 @@ class MovieMapper {
             ? 'https://image.tmdb.org/t/p/w500${theMovieDB.backdropPath}'
             : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
         genreIds: theMovieDB.genreIds.map((e) => e.toString()).toList(),
+        id: theMovieDB.id,
+        originalLanguage: theMovieDB.originalLanguage,
+        originalTitle: theMovieDB.originalTitle,
+        overview: theMovieDB.overview,
+        popularity: theMovieDB.popularity,
+        posterPath: theMovieDB.posterPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${theMovieDB.posterPath}'
+            : '',
+        releaseDate: theMovieDB.releaseDate,
+        title: theMovieDB.title,
+        video: theMovieDB.video,
+        voteAverage: theMovieDB.voteAverage,
+        voteCount: theMovieDB.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails theMovieDB) => Movie(
+        adult: theMovieDB.adult,
+        backdropPath: theMovieDB.backdropPath != ''
+            ? 'https://image.tmdb.org/t/p/w500${theMovieDB.backdropPath}'
+            : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
+        genreIds: theMovieDB.genres.map((e) => e.name).toList(),
         id: theMovieDB.id,
         originalLanguage: theMovieDB.originalLanguage,
         originalTitle: theMovieDB.originalTitle,
