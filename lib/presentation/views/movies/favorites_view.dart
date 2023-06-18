886db0f0ddf590427c00ac7e03341c2c92429584
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-06-18 18:25:34 +0200

    Show message when there no favorite movies added

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -25,12 +26,38 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   Widget build(BuildContext context) {
     final List<Movie> favoriteMovies =
         ref.watch(favoriteMoviesProvider).values.toList();
+    final colors = Theme.of(context).colorScheme;
 
     return Scaffold(
-      body: MovieMasonry(
-        movies: favoriteMovies,
-        loadNextPage: _loadNextPage,
-      ),
+      body: favoriteMovies.isNotEmpty
+          ? MovieMasonry(
+              movies: favoriteMovies,
+              loadNextPage: _loadNextPage,
+            )
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.favorite_outline_sharp,
+                      size: 60, color: colors.primary),
+                  const Text(
+                    'Try adding some movies',
+                    style: TextStyle(fontSize: 20, color: Colors.black45),
+                  ),
+                  const SizedBox(height: 20),
+                  FilledButton.tonal(
+                    onPressed: () => context.go('/home/0'),
+                    child: const Text(
+                      'Search',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
     );
   }
 
